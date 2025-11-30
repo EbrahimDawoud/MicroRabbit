eaa71a6c59ff255a85adbf3d924b6ccadfacf8b6
EbrahimDawoud <ebrahim_EN30895331@f-eng.tanta.edu.eg>
2025-11-30 13:28:26 +0300

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+public class Receiver
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnectionAsync())
+        using (var channel = connection.Result.CreateChannelAsync())
+        {
+            channel.Result.QueueDeclareAsync("BasicTest", false, false, false, null);
+            var consumer = new AsyncEventingBasicConsumer(channel.Result);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+                await Task.Yield();
+            };
+            channel.Result.BasicConsumeAsync(queue: "BasicTest",
+                                     autoAck: true,
+                                     consumer: consumer);
+            Console.WriteLine(" Press [enter] to exit the Consumer...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProducerConsumerRabbitMQ/Sender.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+public class Sender
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnectionAsync())
+
+        using (var channel = connection.Result.CreateChannelAsync())
+        {
+            channel.Result.QueueDeclareAsync("BasicTest",false,  false, false,null);
+            string message = "Getting started with .net Core Rqbbit MQ";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.Result.BasicPublishAsync("", "BasicTest", false,  body);
+            Console.WriteLine(" [x] Sent {0}", message);
+
+        }
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+
+    }
+}
